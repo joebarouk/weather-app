36673894d8ef45d4097399e837a827c0a17fca82
joe <joebarouk@gmail.com>
2022-07-14 13:49:29 +0300

    Network4+fragment

---
+++ app/src/main/java/city/CityFragment.kt
@@ -0,0 +1,36 @@
+package com.example.weatherapp.city
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.weatherapp.R
+import com.example.weatherapp.databinding.FragmentCityBinding
+
+class OverviewFragment : Fragment() {
+
+    /**
+     * Lazily initialize our [OverviewViewModel].
+     */
+    private val viewModel: CityViewModel by lazy {
+        ViewModelProvider(this).get(CityViewModel::class.java)
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentCityBinding.inflate(inflater)
+
+        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+        binding.lifecycleOwner = this
+
+        // Giving the binding access to the OverviewViewModel
+        binding.viewModel = viewModel
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/city/CityViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.weatherapp.city
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weatherapp.network.City
+import com.example.weatherapp.network.CityApi
+import kotlinx.coroutines.launch
+
+enum class CityApiStatus { LOADING, ERROR, DONE }
+
+class CityViewModel : ViewModel() {
+
+
+    private val _status = MutableLiveData<CityApiStatus>()
+    val status: LiveData<CityApiStatus>
+        get() = _status
+
+    private val _cities = MutableLiveData<List<City>>()
+    val properties: LiveData<List<City>>
+        get() = _cities
+
+    private val _get_city_from_text = MutableLiveData<String>()
+    val get_city_from_text: LiveData<String>
+        get() = _get_city_from_text
+
+
+    fun text(){
+        getCities(_get_city_from_text.value)
+    }
+
+    private fun getCities(filter:String?){
+        viewModelScope.launch {
+            _status.value = CityApiStatus.LOADING
+            try {
+                _cities.value = CityApi.retrofitService.getCities(filter!!)
+                _status.value = CityApiStatus.DONE
+            } catch (e: Exception) {
+                _status.value = CityApiStatus.ERROR
+                _cities.value = ArrayList()
+            }
+        }
+
+    }
+
+
+}
