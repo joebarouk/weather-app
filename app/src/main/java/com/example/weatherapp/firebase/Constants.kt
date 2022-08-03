fb71331761a5f39757d6039778650ef6006fc8ff
joe <joebarouk@gmail.com>
2022-08-03 10:03:45 +0300

    successful

--- app/src/main/java/com/example/weatherapp/firebase/Constants.kt
+++ app/src/main/java/com/example/weatherapp/firebase/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
 
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com/"
-        const val SERVER_KEY = "BPeP-BFCGuc8Qa1QNH3c4sEYprUt3bG8xqSmg9jdIvisbqrLs0IJKYD0tyZIMmdyDAHdcDP5pzegve32fsW5Tos"
+        const val SERVER_KEY = "AAAA8eyV0v4:APA91bFl2VdD_QAX9K_xahjSHdG4qvQ7DtywdLPh5UjbzzxIBGmJ60_oLVOKfqp56T1nCQfhltEABtJB6J-pu4hHzCFqigvgemiY1pmUtjQOOfn7O2qa-bXOs6Q04cHjCyvZ9kJy6biI"
         const val CONTENT_TYPE = "application/json"
     }
 }
